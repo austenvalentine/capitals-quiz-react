25faf151a71d26c36057b7c02e0389d550d4ea0f
Austen <connect@austenvalentine.com>
2020-02-06 12:11:00 -0500

    reworking useEffect dependencies

--- src/App.js
+++ src/App.js
@@ -1,80 +1,70 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import RegionHeader from "./components/RegionHeader";
 import CurrentCountry from "./components/CurrentCountry";
 import CapitalsList from "./components/CapitalsList";
 
 // Capitals Quiz - breakdown
-// 1. download list of countries and add to "deck" (App.js)
+// 1. download list of countries and add to the countries array (App.js)
 // 2. pick 4 countries at random for multiple-choice options (App.js)
 // 3. when user selects an option, show correct/incorrect
 // 4. go back to step 2 until fewer than 4 countries remain in the deck
 // 5. go back to step 1
 
 function App() {
-  const optionCount = 4;
+  const numberOfOptions = 4;
 
-  const [countriesDeck, setCountriesDeck] = useState([]);
-  const [capitalsOptions, setCapitalsOptions] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [countryOptions, setCountryOptions] = useState([]);
   // ===============================================
   // 1. download list of countries and add to "deck"
   // ===============================================
   //    define function to get countries and capitals from API
-  function fetchCountries() {
-    axios
-      .get("https://restcountries.eu/rest/v2/region/africa")
-      .then(function(response) {
-        console.log("RESPONSE: ", response.data);
-        const { countries, options } = getNextOptions(response.data);
-        console.log("STATE: ", options, countries);
-        setCountriesDeck(countries);
-        setCapitalsOptions(options);
-      });
+  async function fetchCountries() {
+    const response = await fetch(
+      "https://restcountries.eu/rest/v2/region/africa"
+    );
+    const data = await response.json();
+    setCountries(data);
   }
 
-  // ===============================================
-  // 2. pick 4 countries at random for multiple-choice options
-  // ===============================================
-  // define function to randomly pick 4 countries and remove them from the
-  // countriesDeck array.
-  function getNextOptions(countries) {
-    // an array in which to store the randomly-selected countries
-    const options = [];
-    for (let i = 0; i < optionCount; i++) {
-      const randIndex = Math.floor(Math.random() * countries.length);
-      const randCountry = countries[randIndex];
-      options.push(randCountry);
-      // remove randomly picked country from deck
-      countries = countries.filter(country => {
-        return randCountry !== country;
+  // pick the countries at random
+  function pickCountries() {
+    let _countries = [...countries];
+    const _countryOptions = [];
+    for (let i = 0; i < numberOfOptions; i++) {
+      const randomIndex = Math.floor(Math.random() * _countries.length);
+      const pickedCountry = _countries[randomIndex];
+      _countryOptions.push(pickedCountry);
+      _countries = _countries.filter(country => {
+        return pickedCountry !== country;
       });
     }
-    // return new array of countries and array of options
-    return { countries, options };
+    setCountries(_countries);
+    setCountryOptions(_countryOptions);
   }
 
-  // trigger fetch when countriesDeck is low or empty
-  useEffect(() => {
-    if (countriesDeck.length < optionCount) {
-      fetchCountries();
-    }
+  useEffect(function() {
+    fetchCountries();
+  }, []);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [countriesDeck]);
-
-  // user-triggered question generation
-  const handleClick = () => {
-    const { countries, options } = getNextOptions(countriesDeck);
-    setCountriesDeck(countries);
-    setCapitalsOptions(options);
-  };
+  // handleClickNext
+  function handleClickNext() {
+    (async function() {
+      if (countries.length < numberOfOptions) {
+        await fetchCountries();
+      }
+      pickCountries();
+      console.log("handleClick:", countries, countryOptions);
+    })();
+  }
 
   return (
     <div className="App" style={{ background: "#cc8833", height: "100vh" }}>
       <RegionHeader></RegionHeader>
-      <CurrentCountry country={capitalsOptions[0]}></CurrentCountry>
-      <CapitalsList capitals={capitalsOptions}></CapitalsList>
-      <button onClick={handleClick}>Next Question</button>
+      <CurrentCountry country={countryOptions[0]}></CurrentCountry>
+      <CapitalsList capitals={countryOptions}></CapitalsList>
+      <button onClick={handleClickNext}>Next Question</button>
     </div>
   );
 }
--- src/components/CurrentCountry.js
+++ src/components/CurrentCountry.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function CurrentCountry() {
+function CurrentCountry(props) {
   return (
     <div>
       <h2>Which capital belongs to {"some country"}?</h2>
